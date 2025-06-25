3f37343552e1f39b3f356dab98e2091cf6f35da1
carlossergio55 <warzoneoriental@gmail.com>
2025-06-25 15:31:41 -0400

    actualizacion de asientos
    esoecificacion de asientos

--- 02_Server/Core/Aplicacion/Features/Integracion/Queries/GetAllAsientoQuery.cs
+++ 02_Server/Core/Aplicacion/Features/Integracion/Queries/GetAllAsientoQuery.cs
@@ -1,10 +1,12 @@
 ﻿using Aplicacion.DTOs.Integracion;
 using Aplicacion.Interfaces;
 using Aplicacion.Wrappers;
+using Ardalis.Specification;
 using AutoMapper;
 using Dominio.Entities.Integracion;
 using MediatR;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -31,11 +33,18 @@ namespace Aplicacion.Features.Integracion.Queries
 
             public async Task<Response<List<AsientoDto>>> Handle(GetAllAsientoQuery request, CancellationToken cancellationToken)
             {
-                var _Asiento = await _repositoryAsync.ListAsync();
+                var _Asiento = await _repositoryAsync.ListAsync(new AsientoSpecification(), cancellationToken);
                 var _AsientoDto = _mapper.Map<List<AsientoDto>>(_Asiento);
                 return new Response<List<AsientoDto>>(_AsientoDto);
             }
 
+        }
+        public class AsientoSpecification : Specification<Asiento>
+        {
+            public AsientoSpecification()
+            {
+                Query.Include(x => x.Bus);
+            }
         }
 
     }
